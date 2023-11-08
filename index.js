36ce9eae232e7030f5c974d03826fb66b60bcd67
Akash-Edake <akashedake30@gmail.com>
2023-11-08 22:55:05 +0530

    cors handling

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require("./src/db/mongoose");
 const express = require("express");
+const cors = require("cors");
 const User = require("./src/model/user");
 
 const port = process.env.PORT || 8089;
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.listen(port, () => console.log("local connected"));
 
 app.get("/getusers", async (req, res) => {
