e13b2c5d715b550d450da2b0a06f0550e4dccdac
Akash-Edake <akashedake30@gmail.com>
2023-11-21 07:13:51 +0530

    load balancer-->cluster

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("./src/db/mongoose");
-const cluster = require("node:cluster");
+const cluster = require("cluster");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const userRouter = require("./src/routers/user");
 const codeRouter = require("./src/routers/code");
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 const numCPUs = require("node:os").availableParallelism();
 
@@ -22,14 +27,12 @@ if (cluster.isPrimary) {
 } else {
   const port = process.env.PORT || 8080;
 
-  const app = express();
+
 
   // app.use((req,res,next)=>{
   //     res.status(503).send("server under maintenance")
   // })
-  app.use(express.json());
-  app.use(morgan("dev"));
-  app.use(cors());
+
   app.use(userRouter);
   app.use(codeRouter);
 
