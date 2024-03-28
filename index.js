323c707dceca1aac17955ae2728443ec6d8a1c3e
Akash-Edake <101496957+Akash-Edake@users.noreply.github.com>
2024-03-28 11:21:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const userRouter = require("./src/routers/user");
 const codeRouter = require("./src/routers/code");
 
 const numCPUs = require("node:os").availableParallelism();
-
+  const app = express();
+  app.use(express.json());
+  app.use(morgan("dev"));
+  app.use(cors());
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
 
